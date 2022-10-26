525a4217d7bf62627e4353f1a0d95befe318be46
david-lafontant <lafontant.david@gmail.com>
2022-10-26 02:31:29 -0600

    Setup navbar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './header.css';
 
 function Header() {
   return (
-    <div>header</div>
-  )
+    <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-transparent ms-auto navbar-right">
+      <div className="container-fluid">
+        <a href="www.google.com" className="navbar-brand">Denis&apos; foodies</a>
+        <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <div className="navbar-nav roja">
+            <a href="www.google.com" className="nav-item nav-link">Home</a>
+            <a href="www.google.com" className="nav-item nav-link">Package</a>
+            <a href="www.google.com" className="nav-item nav-link">About us</a>
+            <a href="www.google.com" className="nav-item nav-link">Contact Us</a>
+            <a href="www.google.com" className="nav-item nav-link">Cart</a>
+            <a href="www.google.com" className="nav-item nav-link">Booking Now</a>
+
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Header
+export default Header;
