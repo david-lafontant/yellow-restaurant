412c6d0a952c2b038210d3bc4cac5f00d98f6bcc
david-lafontant <lafontant.david@gmail.com>
2022-10-27 18:44:54 -0600

    Add content to landing page

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="container">
+      <div className="row">
+        <section className="landing">
+          <h2>
+            Your Favorite food delivered hot & fresh!
+          </h2>
+          <p>
+            Healthy switcher chefs do all the prep work like peeding,
+            chopping, and marinating, so you can cook a fresh food.
+          </p>
+          <a href="www.food.com" className="app_button">
+            Order now
+            <FontAwesomeIcon icon={faArrowRight} aria-hidden="true" />
+          </a>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
