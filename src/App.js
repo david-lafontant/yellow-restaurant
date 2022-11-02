f0f79c38d2ef315c2af7e02991746acb806f2c78
david-lafontant <lafontant.david@gmail.com>
2022-11-02 07:41:37 -0600

    Display work component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import About from './components/about/about';
 import Header from './components/header/header';
 import Home from './components/home/home';
+import Work from './components/work/work';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Home />
       <About />
+      <Work />
     </div>
   );
 }
