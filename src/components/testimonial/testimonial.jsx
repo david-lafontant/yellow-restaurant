b4e5e8d7af34473fe8053404c6a72abb66eba206
david-lafontant <lafontant.david@gmail.com>
2022-11-02 15:11:39 -0600

    Add content to testimonial component

--- src/components/testimonial/testimonial.jsx
+++ src/components/testimonial/testimonial.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import witness from '../../assets/pictures/pizza.webp';
 
-function Testimonial () {
+function Testimonial() {
   return (
     <div>
-      <h1>Testimonial</h1>
+      <div>
+        <h2>Testimonial</h2>
+        <h3>What They Are Saying</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at
+          lorem ac justo sagittis volutpat. Curabitur magna tellus, ullamcorper
+          eu leo non, tempor iaculis augue.
+        </p>
+      </div>
+      <div>
+        <img src={witness} alt="witness" />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at
+          lorem ac justo sagittis volutpat. Curabitur magna tellus, ullamcorper
+          eu leo non, tempor iaculis augue.
+        </p>
+        <p>
+          <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+          <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+          <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+          <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+          <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+        </p>
+        <p>
+          Omeletto
+          <small>Egg county</small>
+        </p>
+      </div>
     </div>
   );
 }
