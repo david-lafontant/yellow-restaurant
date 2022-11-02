20e5e6879583cf99c7d20ce1f6e8cf7a4383acc2
david-lafontant <lafontant.david@gmail.com>
2022-11-02 14:38:18 -0600

    Add content to menu

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,9 +1,195 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import rice from '../../assets/pictures/rice.webp';
 
-const Menu = () => {
+function Menu() {
   return (
-    <div>Menu</div>
-  )
+    <div className="row menu-container">
+      <div>
+        <h2>Menu</h2>
+        <h3>explore our best menu</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at
+          lorem ac justo sagittis volutpat. Curabitur magna tellus, ullamcorper
+          eu leo non, tempor iaculis augue.
+        </p>
+      </div>
+      <div>
+        <table>
+          <tr>
+            <td>
+              <img src={rice} alt="rice" />
+              <div>
+                <h3>
+                  <span>Rice Muffen</span>
+                  <span>$15</span>
+                </h3>
+                <p>Served with french fries + drink</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus at lorem ac justo sagittis volutpat.
+                  Curabitur magna tellus, ullamcorper eu leo non, tempor iaculis augue.
+                </p>
+                <p>
+                  <span>
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                  </span>
+                  <span>
+                    <button type="button">+</button>
+                  </span>
+                </p>
+              </div>
+            </td>
+            <td>
+              <img src={rice} alt="rice" />
+              <div>
+                <h3>
+                  <span>Rice Muffen</span>
+                  <span>$15</span>
+                </h3>
+                <p>Served with french fries + drink</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus at lorem ac justo sagittis volutpat.
+                  Curabitur magna tellus, ullamcorper eu leo non, tempor iaculis augue.
+                </p>
+                <p>
+                  <span>
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                  </span>
+                  <span>
+                    <button type="button">+</button>
+                  </span>
+                </p>
+              </div>
+            </td>
+            <td>
+              <img src={rice} alt="rice" />
+              <div>
+                <h3>
+                  <span>Rice Muffen</span>
+                  <span>$15</span>
+                </h3>
+                <p>Served with french fries + drink</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus at lorem ac justo sagittis volutpat.
+                  Curabitur magna tellus, ullamcorper eu leo non, tempor iaculis augue.
+                </p>
+                <p>
+                  <span>
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                  </span>
+                  <span>
+                    <button type="button">+</button>
+                  </span>
+                </p>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src={rice} alt="rice" />
+              <div>
+                <h3>
+                  <span>Rice Muffen</span>
+                  <span>$15</span>
+                </h3>
+                <p>Served with french fries + drink</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus at lorem ac justo sagittis volutpat.
+                  Curabitur magna tellus, ullamcorper eu leo non, tempor iaculis augue.
+                </p>
+                <p>
+                  <span>
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                  </span>
+                  <span>
+                    <button type="button">+</button>
+                  </span>
+                </p>
+              </div>
+            </td>
+            <td>
+              <img src={rice} alt="rice" />
+              <div>
+                <h3>
+                  <span>Rice Muffen</span>
+                  <span>$15</span>
+                </h3>
+                <p>Served with french fries + drink</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus at lorem ac justo sagittis volutpat.
+                  Curabitur magna tellus, ullamcorper eu leo non, tempor iaculis augue.
+                </p>
+                <p>
+                  <span>
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                  </span>
+                  <span>
+                    <button type="button">+</button>
+                  </span>
+                </p>
+              </div>
+            </td>
+            <td>
+              <img src={rice} alt="rice" />
+              <div>
+                <h3>
+                  <span>Rice Muffen</span>
+                  <span>$15</span>
+                </h3>
+                <p>Served with french fries + drink</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus at lorem ac justo sagittis volutpat.
+                  Curabitur magna tellus, ullamcorper eu leo non, tempor iaculis augue.
+                </p>
+                <p>
+                  <span>
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                    <FontAwesomeIcon icon={faStar} aria-hidden="true" />
+                  </span>
+                  <span>
+                    <button type="button">+</button>
+                  </span>
+                </p>
+              </div>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div>
+        <a href="www.google.com">Load More</a>
+      </div>
+    </div>
+  );
 }
 
 export default Menu;
