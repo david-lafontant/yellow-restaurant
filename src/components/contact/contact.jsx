7897088bec066ba8f27b5ec682ac8fae316bf637
david-lafontant <lafontant.david@gmail.com>
2022-11-04 19:54:59 -0600

    Add content to form

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 function Contact() {
+  const [email, setEmail] = useState('');
   return (
-    <div>Contact</div>
+    <div className="row contact">
+      <div>
+        <h2>Contact</h2>
+      </div>
+      <div>
+        <form>
+          <label for="email" className="label">
+            <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+          </label>
+        </form>
+      </div>
+      <div>
+        <ul>
+          <li>Youtube</li>
+          <li>facebook</li>
+          <li>Tweeter</li>
+          <li>instagram</li>
+        </ul>
+      </div>
+
+
+
+    </div>
   )
 }
 
