ec215b0505bb2438f953c43133a7f7864f213e86
david-lafontant <lafontant.david@gmail.com>
2022-11-06 05:26:47 -0600

    Add picture to home page

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import './home.css'
+import './home.css';
+import riceSalad from '../../assets/pictures/rice_salad.webp';
 
 function Home() {
   return (
-    <div className="container landing">
-      <div className="row">
-        <section className="landing">
-          <h2>
-            Your Favorite food delivered hot & fresh!
-          </h2>
-          <p>
-            Healthy switcher chefs do all the prep work like peeding,
-            chopping, and marinating, so you can cook a fresh food.
-          </p>
-          <a href="www.food.com" className="app_button">
-            Order now
-            <FontAwesomeIcon icon={faArrowRight} aria-hidden="true" />
-          </a>
-        </section>
+    <section className="row">
+      <div className="col-md-6">
+        <h2>
+          Your Favorite food delivered hot & fresh!
+        </h2>
+        <p>
+          Healthy switcher chefs do all the prep work like peeding,
+          chopping, and marinating, so you can cook a fresh food.
+        </p>
+        <a href="www.food.com" className="app_button">
+          Order now
+          <FontAwesomeIcon icon={faArrowRight} aria-hidden="true" />
+        </a>
       </div>
-    </div>
+      <div className="col-md-6">
+        <img src={riceSalad} alt="rice salad" />
+      </div>
+    </section>
   );
 }
 
