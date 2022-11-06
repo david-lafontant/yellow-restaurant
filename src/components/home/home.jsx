78c1da582471edbf76853091b72e100c0da5a7ca
david-lafontant <lafontant.david@gmail.com>
2022-11-06 06:10:07 -0600

    Add info card

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowRight,
+  faClock,
+  faLocationDot,
+  faPhone,
+} from '@fortawesome/free-solid-svg-icons';
 import './home.css';
 import riceSalad from '../../assets/pictures/rice_salad.webp';
 
 function Home() {
   const style = {
     height: '100vh',
+  };
+  const cardWidth = {
+    width: '18rem',
   };
   return (
     <section className="row align-items-center" style={style}>
@@ -27,7 +35,29 @@ function Home() {
         <img src={riceSalad} alt="rice salad" />
       </div>
       <div className="col-12 border info">
- 
+        <div className="card-group">
+          <div className="card" style={cardWidth}>
+            <FontAwesomeIcon icon={faClock} aria-hidden="true" className="card-img-top" />
+            <div className="card-body">
+              <h3>Today 10:00 am - 7:00 pm</h3>
+              <p>Working Hours</p>
+            </div>
+          </div>
+          <div className="card" style={cardWidth}>
+            <FontAwesomeIcon icon={faLocationDot} aria-hidden="true" className="card-img-top" />
+            <div className="card-body">
+              <h3>Santiago de Los Caballeros</h3>
+              <p>Get Directions</p>
+            </div>
+          </div>
+          <div className="card" style={cardWidth}>
+            <FontAwesomeIcon icon={faPhone} aria-hidden="true" className="card-img-top" />
+            <div className="card-body">
+              <h3>+1(809) 999 99 99</h3>
+              <p>Working Hours</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
