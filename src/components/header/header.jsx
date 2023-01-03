db0b57afabb1eda1bcdd886a84bda89e254b248f
david-lafontant <lafontant.david@gmail.com>
2023-01-03 16:49:35 -0600

    Fix linter errors in header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,23 +8,23 @@ function Header() {
   return (
     <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-transparent ms-auto navbar-right">
       <div className="container-fluid">
-        <Link to='/' className="navbar-brand brand">Denis&apos; foodies</Link>
+        <Link to="/" className="navbar-brand brand">Denis&apos; foodies</Link>
         <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <div className="navbar-nav roja">
-            <Link to='/' className="nav-item nav-link">Home</Link>
-            <Link to='/menu' className="nav-item nav-link">
+            <Link to="/" className="nav-item nav-link">Home</Link>
+            <Link to="/menu" className="nav-item nav-link">
               Menu
             </Link>
-            <Link to='/about' className="nav-item nav-link">About us</Link>
-            <Link to='/work' className="nav-item nav-link">Work</Link>
-            <Link to='/testimonials' className="nav-item nav-link"></Link>
+            <Link to="/about" className="nav-item nav-link">About us</Link>
+            <Link to="/work" className="nav-item nav-link">Work</Link>
+            <Link to="/testimonials" className="nav-item nav-link">Testimonial</Link>
             <a href="www.google.com" className="nav-item nav-link">
               <FontAwesomeIcon icon={faShoppingCart} aria-hidden="true" />
             </a>
-            <Link to='/contact' className="nav-item nav-link">Booking Now</Link>
+            <Link to="/contact" className="nav-item nav-link">Booking Now</Link>
 
           </div>
         </div>
