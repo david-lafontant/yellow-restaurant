0c753227157749cf7468242a103e092cba1c21eb
david-lafontant <lafontant.david@gmail.com>
2023-01-03 16:41:59 -0600

    Use Link to setup links in header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
@@ -7,22 +8,23 @@ function Header() {
   return (
     <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-transparent ms-auto navbar-right">
       <div className="container-fluid">
-        <a href="www.google.com" className="navbar-brand brand">Denis&apos; foodies</a>
+        <Link to='/' className="navbar-brand brand">Denis&apos; foodies</Link>
         <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <div className="navbar-nav roja">
-            <a href="www.google.com" className="nav-item nav-link">Home</a>
-            <a href="www.google.com" className="nav-item nav-link">
-              Package
-            </a>
-            <a href="www.google.com" className="nav-item nav-link">About us</a>
-            <a href="www.google.com" className="nav-item nav-link">Contact Us</a>
+            <Link to='/' className="nav-item nav-link">Home</Link>
+            <Link to='/menu' className="nav-item nav-link">
+              Menu
+            </Link>
+            <Link to='/about' className="nav-item nav-link">About us</Link>
+            <Link to='/work' className="nav-item nav-link">Work</Link>
+            <Link to='/testimonials' className="nav-item nav-link"></Link>
             <a href="www.google.com" className="nav-item nav-link">
               <FontAwesomeIcon icon={faShoppingCart} aria-hidden="true" />
             </a>
-            <a href="www.google.com" className="nav-item nav-link">Booking Now</a>
+            <Link to='/contact' className="nav-item nav-link">Booking Now</Link>
 
           </div>
         </div>
